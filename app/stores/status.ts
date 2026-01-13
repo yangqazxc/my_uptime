71e0a018e4e25c28c304ee59bb0d5b751f9a4acb
yangqazxc <167492815+yangqazxc@users.noreply.github.com>
2026-01-13 12:15:32 +0800

    Add files via upload

--- app/stores/status.ts
+++ app/stores/status.ts
@@ -1,25 +1,17 @@
 import { defineStore } from "pinia";
 import type { MonitorsDataResult, SiteLangType, SiteType } from "~~/types/main";
 
-export const useStatusStore = defineStore(
-  "status",
-  () => {
-    // 登录状态
-    const loginStatus = ref<boolean>(false);
-    // 站点状态
-    const siteStatus = ref<SiteType>("loading");
-    // 站点数据
-    const siteData = ref<MonitorsDataResult>();
-    // 滚动高度
-    const scrollTop = ref<number>(0);
-    // 站点语言
-    const siteLang = ref<SiteLangType>("zh-CN");
+export const useStatusStore = defineStore("status", () => {
+  // 登录状态
+  const loginStatus = ref<boolean>(false);
+  // 站点状态
+  const siteStatus = ref<SiteType>("loading");
+  // 站点数据
+  const siteData = ref<MonitorsDataResult>();
+  // 滚动高度
+  const scrollTop = ref<number>(0);
+  // 站点语言
+  const siteLang = ref<SiteLangType>("zh-CN");
 
-    return { loginStatus, siteStatus, siteData, scrollTop, siteLang };
-  },
-  {
-    persist: {
-      paths: ["siteLang"],
-    },
-  },
-);
+  return { loginStatus, siteStatus, siteData, scrollTop, siteLang };
+});
